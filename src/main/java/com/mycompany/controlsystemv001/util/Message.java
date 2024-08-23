a646cb7e5c0975e2b96ef8089dba549ffa161180
leandromacedosilva <iamfulldeveloper@gmail.com>
2024-08-23 20:56:57 -0300

    Update file cadastroproduto.xhtml and file cadastroproduto.css in project in Github repository

--- src/main/java/com/mycompany/controlsystemv001/util/Message.java
+++ src/main/java/com/mycompany/controlsystemv001/util/Message.java
@@ -1,9 +1,13 @@
 package com.mycompany.controlsystemv001.util;
 
+import jakarta.faces.context.FacesContext;
+
 /**
  *
  * @author leandro
  */
 public class Message {
-    
+    public static void mesage() {
+        //FacesContext.
+    }
 }
